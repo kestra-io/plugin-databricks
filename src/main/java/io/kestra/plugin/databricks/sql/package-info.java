a081a97a186bea890320045d39a4417aea92f651
Loïc Mathieu <loikeseke@gmail.com>
2023-07-17 14:19:35 +0200

    feat: add icons and descriptions

---
+++ src/main/java/io/kestra/plugin/databricks/sql/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Databricks",
+    description = "This sub-group of plugins contains tasks for executing SQL queries on a Databricks cluster.",
+    categories = { PluginSubGroup.PluginCategory.TOOL, PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.databricks.sql;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
